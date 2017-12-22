c2a12a69e084a4fb28d21ace7003ab24c02d67c8
Ardi Baskoro <mistersemar.ab@gmail.com>
2017-12-23 02:13:49 +0700

    Initial commit

---
+++ app/src/main/java/id/gifood/placepicker/MainActivity.java
@@ -0,0 +1,58 @@
+package id.gifood.placepicker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnMaps;
+    private TextView tvPlace;
+    private int PLACE_PICKER_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvPlace = findViewById(R.id.main_place);
+        btnMaps= findViewById(R.id.main_maps);
+
+        btnMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // menangkap hasil balikan dari Place Picker, dan menampilkannya pada TextView
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format(
+                        "Place: %s \n" +
+                                "Alamat: %s \n" +
+                                "Latlng %s \n", place.getName(), place.getAddress(), place.getLatLng().latitude + " " + place.getLatLng().longitude);
+                tvPlace.setText(toastMsg);
+            }
+        }
+    }
+}
